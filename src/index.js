f9354ccc18eedad4c4c717ad2da6a66f86b63bd8
Serhii B <s_blyshchak@ukr.net>
2021-12-11 10:28:43 +0200

    router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    
-      <App />
-    
-  </React.StrictMode>,
+  <HashRouter>
+    <React.StrictMode>
+      
+        <App />
+      
+    </React.StrictMode>
+  </HashRouter>,
   document.getElementById('root')
 );
 
