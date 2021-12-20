e32ff447837869f09834897d6a251ca8a7dbf8ee
Serhii B <s_blyshchak@ukr.net>
2021-12-20 02:17:26 +0200

    implement add place

--- src/components/CustomMap.jsx
+++ src/components/CustomMap.jsx
@@ -11,67 +11,112 @@ const mapStyles = {
 }
 
 
-
 const CustomMap = ({google}) => {
 
-const [locations, setLocations] = useState([])
-const [locationId, setLocationId] = useState(null)
-const [locationInfo, setLocationInfo] = useState(null)
-const [locationImage, setLocationImage] = useState(null)
+  const [flag, setFlag] = useState(false)
 
+  const [locations, setLocations] = useState([])
+  const [locationId, setLocationId] = useState(null)
+  const [locationInfo, setLocationInfo] = useState(null)
+  const [locationImage, setLocationImage] = useState(null)
 
-  // useEffect(() => {
-  //   setLocations(locationsData )
-  // }, [])
+  console.log('locations', locations)
+  const [ref, setRef] = useState('')
+  console.log('ref', ref)
 
-  useEffect(() => {
-    axios.get(process.env.REACT_APP_API_URL)
+ 
+    useEffect(() => {
+      axios.get(process.env.REACT_APP_API_URL)
           .then(res => setLocations(JSON.parse(res.data.positions)))
-         
-  }, [])
-  
+    }, [])
 
-  useEffect(() => {
-    if (locationId) {
-      const location = locations.find(item => item.id === locationId)
-      setLocationInfo(location.info )
-      setLocationImage(location.image)
-    }
-    
-  }, [locationId])
-
-  console.log(locationId)
+    useEffect(() => {
+      const timer = setTimeout(() => 
+        axios.get(process.env.REACT_APP_API_URL)
+          .then(res => setLocations(JSON.parse(res.data.positions)))
+      , 3000)
+      
+    }, [flag])
   
+ 
+
+    // useEffect(() => {
+    //  alert('yi')
+
+
+    // }, [locationId])
+
+    // console.log(locationId)
+
+    // const handleClick = (e) => {
+     
+    //   const first = locations.find(item => item.id === locationId)
+    //   locations.filter(item => item.id === locationId)
+    //   const newLocations = [ first, ...locations]
+    //   setLocations(newLocations)
+    // }
+
+
   return (
     <div style={{display:"flex", justifyContent:'space-between' , marginTop:"20px", height:"100%"}}>
-      <Navbar />
-      <div style={{width: "800px",height: "500px",}}>
 
-        <Map 
+      <Navbar flag={flag} setFlag={setFlag} />
+
+      <div style={{width: "800px",height: "500px",}}>
+       
+        <Map
+        id="map"
+          className="map" 
           google={google}
           zoom={11}
           style={mapStyles}
           initialCenter={{ lat: 50.44055686944465, lng: 30.53022771561217 }}
+          ref={map => setRef(map)}
+
+          onReady={(map) => {
+            console.log('onReady', ref.map.getBounds())
+          }}
+
+          onBoundsChange={(e)=>{
+            console.log('onBoundsChange', ref.map.getBounds())
+          }}
+
+          onClick={(e) => {
+            console.log('map bounds', ref.map.getBounds())
+            console.log(ref.map.getBounds().contains(new google.maps.LatLng(locations.find(item => item.id=5).lat, locations.find(item => item.id=5).lng)))    
+          }}
+          
         >
-        {locations.map(
-            loc =>  <Marker key={loc.id} position={{ lat: loc.lat, lng: loc.lng }} onClick={() => setLocationId(loc.id)} />
-            )}
-
+        
+        {
+        locations.map(loc =>  
+            <Marker key={loc.id}  position={{ lat: loc.lat, lng: loc.lng }}  
+           // onClick={() => setLocationId(loc.id)} 
+            />
+            
+        )}
         </Map>
+        
       </div>
-      <div style={{width: "300px", height: "300px"}}>
-        <div>Info about place</div>
-        <img style={{width: "300px", height: "300px"}} src={locationImage} alt="" />
-              
-        <div style={{}}>{locationInfo} </div>
-      </div>
-    </div>
-    
-  )
+      <div  style={{width: "300px", border: "solid black 1px"}}>
+        
+        {
+          locations.map(loc => 
+            <div key={loc.id} style={{ paddingBottom: "30px"}}>
+              <div >
+                <img style={{width: "300px", height: "300px"}} src={loc.image} alt="image" />
+              </div>
+              <div style={{margin: "5px"}}>{loc.info} </div>
 
+            </div>
+          )
+        }
+        </div>
+    </div>  
+  )
 }
 
 
  export default GoogleApiWrapper({
- apiKey: "AIzaSyBEoYmhsxCFMlPtJaP_4B2SSjRDhtweDf0",
+ apiKey: process.env.API_KEY,
  })(CustomMap);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,127 @@
-import React  from 'react'
+import React, { useState }  from 'react'
+import axios from 'axios'
+
+const sendData = async (lat, lng, info, image) => {
+    try {
+        const response = await axios.post('http://localhost:5000/', {
+        lat,
+        lng,
+        info,
+        image
+        })
+        alert(response.data.message)
+    } catch (e) {
+
+    }
+    
+}
 
 
+const Navbar = (props) => {
 
-const Navbar = () => {
+    const [lat, setLat] = useState('')
+    const [lng, setLng] = useState('')
+    const [info, setInfo] = useState('')
+    const [image, setImage] = useState('')
+
+    const handleLat = (e) => {
+        setLat(e.target.value.split(' ').join(''))
+    }
+
+    const handleLng = (e) => {
+        setLng(e.target.value.split(' ').join(''))
+    }
+
+    const handleInfo = (e) => {
+        setInfo(e.target.value.split(' ').join(''))
+    }
+
+    const handleImage = (e) => {
+        setImage(e.target.value.split(' ').join(''))
+    }
+
+    const handleClick =  (e) => {
+        e.preventDefault()
+
+        const obj = {
+            lat : lat,
+            lng: lng,
+            info: info,
+            image: image
+        }
+        console.log(obj)
+        
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*",
+            }
+          }
+          
+        axios.post(process.env.REACT_APP_API_URL, obj, axiosConfig)
+          .then((res) => {
+            console.log("RESPONSE RECEIVED: ", res);
+          })
+          .catch((err) => {
+            console.log("AXIOS ERROR: ", err);
+          })
+       
+        
+        setLat('')
+        setLng('')
+        setInfo('')
+        setImage('')
+        props.setFlag(!props.flag)
+    }
+
 
     return (
         <div style={{ height:"500px", width:"6em", backgroundColor: 'grey', textAlign: "center", fontSize: "40px"}}>
-            Navbar
+            
+            <form >
+                    
+                    <label >
+                    Широта:
+                    <input
+                        value={lat}
+                        type="text" 
+                        
+                        onChange={handleLat}
+                       />
+                    </label >
+                   
+                    <label >
+                    Довгота:
+                    <input
+                        value={lng}
+                        type="text" 
+                        
+                        onChange={handleLng}
+                       />
+                    </label >
+                    <label >
+                    
+                    Інформація:
+                    <input
+                        value={info}
+                        type="text" 
+                       
+                        onChange={handleInfo}
+                       />
+                    </label >
+                    <label>
+                    Фото (Url):
+                    <input
+                        value={image}
+                        type="text" 
+                        
+                        onChange={handleImage}
+                       />
+                    </label >
+
+                    <button onClick={handleClick  }>Здати в оренду</button>  
+                   
+            </form>
         </div>
     )
 }
